3a0dabdaa4c37c4c0bd2c5b4181a3a78699bcec6
John Molloy <John@Johns-MacBook-Pro.local>
2015-04-15 13:32:43 +0100

    Search v1
    Stable search v1

---
+++ app/src/main/java/com/example/john/myapplication/Search.java
@@ -0,0 +1,213 @@
+package com.example.john.myapplication;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+
+public class Search extends Activity implements AdapterView.OnItemSelectedListener{
+
+    private String SEARCH_URL;
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    private ProgressDialog pDialog;
+
+    JSONParser jsonParser = new JSONParser();
+
+    private String age,type,music_genre,name;
+    private String date = "-1";
+
+    private EditText txtName;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        SEARCH_URL= getString(R.string.url_start) + "search.php";
+
+        txtName = (EditText)findViewById(R.id.txtEventName);
+
+        Spinner spinner = (Spinner) findViewById(R.id.event_type_spinner);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.event_type_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Spinner ageSpinner = (Spinner) findViewById(R.id.event_age_spinner);
+        ageSpinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> ageAdapter = ArrayAdapter.createFromResource(this,
+                R.array.event_age_array, android.R.layout.simple_spinner_item);
+        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ageSpinner.setAdapter(ageAdapter);
+
+        Spinner musicSpinner = (Spinner) findViewById(R.id.music_genre_spinner);
+        musicSpinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> musicAdapter = ArrayAdapter.createFromResource(this,
+                R.array.event_music_array, android.R.layout.simple_spinner_item);
+        musicAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        musicSpinner.setAdapter(musicAdapter);
+
+
+    }
+
+    public void submitSearch(View v){
+        name = txtName.getText().toString();
+        System.out.println("Clicked the search button");
+        System.out.println("Type: "+type+" Genre: "+music_genre+" Age: "+age+" Date: "+date+" Name: "+name);
+
+        Intent i = new Intent(Search.this, SearchList.class);
+
+        i.putExtra("event_type", type);
+        i.putExtra("music_genre", music_genre);
+        i.putExtra("age", age);
+        i.putExtra("date", date);
+        i.putExtra("name", name);
+
+        startActivity(i);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch(parent.getId()) {
+            case R.id.event_type_spinner:
+                type = parent.getItemAtPosition(position).toString();
+                System.out.println("SELECTED: " + type);
+                break;
+            case R.id.event_age_spinner:
+                age = parent.getItemAtPosition(position).toString();
+                System.out.println("SELECTED: " + age);
+                break;
+            case R.id.music_genre_spinner:
+                music_genre = parent.getItemAtPosition(position).toString();
+                System.out.println("SELECTED: " + music_genre);
+        }
+
+    }
+
+
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            monthOfYear +=1;
+            String inputDate = year + "-" + checkDate(monthOfYear) + "-" + checkDate(dayOfMonth);
+            ((Search)getActivity()).onDateSet(inputDate);
+          //  System.out.println("DATE: " + inputDate);
+        }
+    }
+
+    public void onDateSet(String date){
+        this.date = date;
+    }
+
+    //Pads day and month with a leading zero for date comparison in database
+    public static String checkDate(int num){
+        return num <= 9?"0"+num:String.valueOf(num);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void openHomeActivity(View v)
+    {
+        Intent i = new Intent(Search.this, Home.class);
+        startActivity(i);
+    }
+
+    public void openSearchActivity(View v)
+    {
+        Intent i = new Intent(Search.this, Search.class);
+        startActivity(i);
+    }
+
+    public void openUsersActivity(View v)
+    {
+        Intent i = new Intent(Search.this, Users.class);
+        startActivity(i);
+    }
+
+    public void openSettingsActivity(View v)
+    {
+        Intent i = new Intent(Search.this, Settings.class);
+        startActivity(i);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
--- app/src/main/java/com/example/john/myapplication/SearchList.java
+++ app/src/main/java/com/example/john/myapplication/SearchList.java
@@ -1,19 +1,291 @@
 package com.example.john.myapplication;
 
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
-public class SearchList extends ActionBarActivity {
+public class SearchList extends ListActivity {
+
+    private String SEARCH_URL;
+
+    private static final String TAG_POSTS = "posts";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_DATE = "date";
+    private static final String TAG_VENUE = "venue";
+    private static final String TAG_TYPE = "type";
+    private static final String TAG_ID = "event_id";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    private ProgressDialog pDialog;
+
+    JSONParser jsonParser = new JSONParser();
+    private JSONArray mComments = null;
+
+    private ArrayList<HashMap<String, String>> mCommentList;
+
+    private String age,type,music_genre,name,date;
+
+    private int success;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_list);
+
+        SEARCH_URL= getString(R.string.url_start) + "eventsearch.php";
+
+        Intent i = getIntent();
+        type = i.getStringExtra("event_type");
+        music_genre = i.getStringExtra("music_genre");
+        age = i.getStringExtra("age");
+        date = i.getStringExtra("date");
+        name = i.getStringExtra("name");
+
+        System.out.println("Type: "+type+" Genre: "+music_genre+" Age: "+age+" Date: "+date+" Name: "+name);
+
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //loading the comments via AsyncTask
+        new LoadSearchResults().execute();
+    }
+
+    public class LoadSearchResults extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(SearchList.this);
+            pDialog.setMessage("Loading Search Results...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... arg0) {
+            //we will develop this method in version 2
+
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("event_type", type));
+                params.add(new BasicNameValuePair("music_genre", music_genre));
+                params.add(new BasicNameValuePair("age", age));
+                params.add(new BasicNameValuePair("date", date));
+                params.add(new BasicNameValuePair("name", name));
+
+                Log.d("request!", "starting");
+                // getting product details by making HTTP request
+                JSONObject json = jsonParser.makeHttpRequest(SEARCH_URL, "POST", params);
+
+                // check your log for json response
+                Log.d("Login attempt", json.toString());
+
+                // json success tag
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("Search Successful!", json.toString());
+
+                    /*SharedPreferences sp = PreferenceManager
+                            .getDefaultSharedPreferences(MainActivity.this);
+                    SharedPreferences.Editor edit = sp.edit();
+                    edit.putString("username", username);
+                    edit.commit();*/
+
+                    /*Intent i = new Intent(MainActivity.this, Home.class);
+                    finish();
+                    startActivity(i);
+                    return json.getString(TAG_MESSAGE);*/
+                    mComments = json.getJSONArray(TAG_POSTS);
+                    updateJSONdata();
+                }else{
+                    Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+                    //return json.getString(TAG_MESSAGE);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+            pDialog.dismiss();
+            //we will develop this method in version 2
+            updateList();
+        }
+    }
+
+    public void updateJSONdata() {
+        System.out.println("Got to updateJSONdata method.");
+        // Instantiate the arraylist to contain all the JSON data.
+        // we are going to use a bunch of key-value pairs, referring
+        // to the json element name, and the content, for example,
+        // message it the tag, and "I'm awesome" as the content..
+
+        mCommentList = new ArrayList<HashMap<String, String>>();
+
+        // Bro, it's time to power up the J parser
+
+        //JSONParser jParser = new JSONParser();
+        // Feed the beast our comments url, and it spits us
+        //back a JSON object.  Boo-yeah Jerome.
+
+        //JSONObject json = jParser.getJSONFromUrl(SEARCH_URL);
+
+        //when parsing JSON stuff, we should probably
+        //try to catch any exceptions:
+        try {
+
+            //I know I said we would check if "Posts were Avail." (success==1)
+            //before we tried to read the individual posts, but I lied...
+            //mComments will tell us how many "posts" or comments are
+            //available
+            //mComments = json.getJSONArray(TAG_POSTS);
+
+            // looping through all posts according to the json object returned
+            for (int i = 0; i < mComments.length(); i++) {
+                JSONObject c = mComments.getJSONObject(i);
+
+                //gets the content of each tag
+                String id = c.getString(TAG_ID);
+                String name = c.getString(TAG_NAME);
+                String date = c.getString(TAG_DATE);
+                String venue = c.getString(TAG_VENUE);
+
+                System.out.println("GOT ARRAY: Title: " + name + " Date: " + date + " Venue: " + venue + " ID: " + id);
+
+
+                // creating new HashMap
+                HashMap<String, String> map = new HashMap<String, String>();
+
+                map.put(TAG_ID, id);
+                map.put(TAG_NAME, name);
+                map.put(TAG_DATE, date);
+                map.put(TAG_VENUE, venue);
+
+                // adding HashList to ArrayList
+                mCommentList.add(map);
+
+                //annndddd, our JSON data is up to date same with our array list
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        Collections.sort(mCommentList, new MapComparator("date"));
+        System.out.println("Out of updateJSONdata method.");
+
+    }
+
+    //To compare map values when sorting
+    class MapComparator implements Comparator<Map<String,String>> {
+        private final String key;
+
+        public MapComparator(String key){
+            this.key = key;
+        }
+
+        @Override
+        public int compare(Map<String, String> lhs, Map<String, String> rhs) {
+            String first = lhs.get(key);
+            String second = rhs.get(key);
+            return first.compareTo(second);
+        }
+    }
+
+    private void updateList() {
+        System.out.println("Got to updateList method.");
+        // For a ListActivity we need to set the List Adapter, and in order to do
+        //that, we need to create a ListAdapter.  This SimpleAdapter,
+        //will utilize our updated Hashmapped ArrayList,
+        //use our single_post xml template for each item in our list,
+        //and place the appropriate info from the list to the
+        //correct GUI id.  Order is important here.
+        if(success ==1) {
+            ListAdapter adapter = new SimpleAdapter(this, mCommentList,
+                    R.layout.single_post, new String[]{TAG_NAME,
+                    TAG_VENUE, TAG_DATE}, new int[]{R.id.title, R.id.message,
+                    R.id.username});
+            System.out.println("Created adapter.");
+            // I shouldn't have to comment on this one:
+            try {
+                setListAdapter(adapter);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Set adapter.");
+
+            // Optional: when the user clicks a list item we
+            //could do something.  However, we will choose
+            //to do nothing...
+            ListView lv = getListView();
+            System.out.println("Init list view.");
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+
+                    // This method is triggered if an item is click within our
+                    // list. For our example we won't be using this, but
+                    // it is useful to know in real life applications.
+                    Integer e_id = (int) (long) id;
+                    HashMap<String, String> test = new HashMap<>(mCommentList.get(e_id));
+                    System.out.println("You clicked me!" + " Event ID: " + test.get(TAG_ID));
+
+
+                    Intent i = new Intent(SearchList.this, SingleEvent.class);
+
+                    i.putExtra("event_id", test.get(TAG_ID));
+                    startActivity(i);
+
+
+                }
+            });
+        }
+        else{}
+        System.out.println("Out of updateList method.");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
